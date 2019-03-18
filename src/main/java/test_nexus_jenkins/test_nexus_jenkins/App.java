b53bcfa8144d668e0459510b2744aa8d2fcd502c
Alexandre_cours <alex.ayato@gmail.com>
2019-03-18 12:35:45 +0100

    zzqd

---
+++ src/main/java/test_nexus_jenkins/test_nexus_jenkins/App.java
@@ -0,0 +1,13 @@
+package test_nexus_jenkins.test_nexus_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
